e4e7fc90a3692dac06c9f66b6c4407033af14c80
Massimo Ambrosino <massimo.ambrosino@outlook.it>
2022-12-28 15:31:04 +0100

    adding interactive tap

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,15 @@ class _RandomWordsState extends State<RandomWords> {
             color: alreadySaved ? Colors.red : null,
             semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
           ),
+          onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                }else{
+                  _saved.add(_suggestions[index]);
+                }
+              });
+          },
         );
         /*oldReturn of ListView*/ //return Text(_suggestions[index].asPascalCase);
       }
